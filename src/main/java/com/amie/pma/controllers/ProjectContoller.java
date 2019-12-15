c82b6bcc4abc859560f436297c03ac639133a746
Amie Thao <amie.c.thao@gmail.com>
2019-12-15 11:35:15 -0600

    add ProjectController class

--- src/main/java/com/amie/pma/controllers/ProjectContoller.java
+++ src/main/java/com/amie/pma/controllers/ProjectContoller.java
@@ -1,5 +1,15 @@
 package com.amie.pma.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/project")
 public class ProjectContoller {
+	
+	@RequestMapping("/new")
+	public String displayProjectForm() {
+		return "new-project";
+	}
 
 }
--- src/main/java/com/amie/pma/entities/Project.java
+++ src/main/java/com/amie/pma/entities/Project.java
@@ -1,5 +1,59 @@
 package com.amie.pma.entities;
 
-public class Project {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+
+@Entity
+public class Project {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long projectId;
+	private String name;
+	private String stage;
+	private String description;
+	
+	// constructor with no values - use to create instance of project without giving any values
+	public Project() {
+		
+	}
+	
+	public Project(String name, String stage, String description) {
+		super();
+		this.name = name;
+		this.stage = stage;
+		this.description = description;
+	}
+	
+	// getters and setters
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getStage() {
+		return stage;
+	}
+	
+	public void setStage(String stage) {
+		this.stage = stage;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
 }
+
+
