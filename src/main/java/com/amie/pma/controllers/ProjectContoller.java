ec776efd943d3a15794621cae0c173298a579376
Amie Thao <amie.c.thao@gmail.com>
2019-12-15 12:15:18 -0600

    create Crud repo interface

--- src/main/java/com/amie/pma/controllers/ProjectContoller.java
+++ src/main/java/com/amie/pma/controllers/ProjectContoller.java
@@ -2,6 +2,8 @@ package com.amie.pma.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.amie.pma.entities.Project;
@@ -10,14 +12,20 @@ import com.amie.pma.entities.Project;
 @RequestMapping("/project") // Home route URL
 public class ProjectContoller {
 	
-	@RequestMapping("/new")
+	@GetMapping("/new-form")
 	public String displayProjectForm(Model model) {
 		
 		//create an instance of Project and bind an empty project form to the object
 		Project oneProject = new Project();
 		//	bind model to HTML view
 		model.addAttribute("project", oneProject);
+		
 		return "new-project";
+	}
+	
+	@PostMapping("/save-project")
+	public String createProjectModel(Project project, Model model) {
+		// this handle saving to the data input from the user to the database
 	}
 
 }
---
+++ src/main/java/com/amie/pma/dao/ProjectRepository.java
@@ -0,0 +1,5 @@
+package com.amie.pma.dao;
+
+public interface ProjectRepository {
+
+}
