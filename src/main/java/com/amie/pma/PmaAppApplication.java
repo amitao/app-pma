d542071f9c5418daf732012608b48dab44ba1c67
Amie Thao <amie.c.thao@gmail.com>
2019-12-15 11:10:28 -0600

    initial commit

---
+++ src/main/java/com/amie/pma/PmaAppApplication.java
@@ -0,0 +1,13 @@
+package com.amie.pma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amie/pma/PmaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amie.pma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
