7a66a641df0cfc383d0488561766786c29899607
Amie Thao <amie.c.thao@gmail.com>
2019-12-15 11:54:44 -0600

    add Model and bind html form to ProjectController

--- src/main/java/com/amie/pma/controllers/ProjectContoller.java
+++ src/main/java/com/amie/pma/controllers/ProjectContoller.java
@@ -1,14 +1,22 @@
 package com.amie.pma.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.amie.pma.entities.Project;
+
 @Controller
-@RequestMapping("/project")
+@RequestMapping("/project") // Home route URL
 public class ProjectContoller {
 	
 	@RequestMapping("/new")
-	public String displayProjectForm() {
+	public String displayProjectForm(Model model) {
+		
+		//create an instance of Project and bind an empty project form to the object
+		Project oneProject = new Project();
+		//	bind model to HTML view
+		model.addAttribute("project", oneProject);
 		return "new-project";
 	}
 
